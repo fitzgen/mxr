1108088fd5bc4eebb75e92c5021083495285e46d
Nick Fitzgerald <fitzgen@gmail.com>
2011-07-28 22:54:27 -0700

    Forgot to fix setup.py before pushing :-/

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-import sys
+from setuptools import setup
 
-print sys.path
-
-
-# from setuptools import setup
-
-# setup(name="mxr",
-#       version="0.0",
-#       description="A command line utlity for using the Mozilla Cross Reference.",
-#       author="Nick Fitzgerald",
-#       author_email="fitzgen@gmail.com",
-#       packages=["mxr"],
-#       scripts=["bin/mxr"],
-#       requires=["lxml"],
-#       license="MIT")
+setup(name="mxr",
+      version="0.0",
+      description="A command line utlity for using the Mozilla Cross Reference.",
+      author="Nick Fitzgerald",
+      author_email="fitzgen@gmail.com",
+      packages=["mxr"],
+      scripts=["bin/mxr"],
+      license="MIT")
